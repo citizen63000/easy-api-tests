cf237867eb2d2cdc7e949790a94628255e7be7c1
lionel <ngconnected@gmail.com>
2022-02-21 20:12:26 +0100

    structure

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace EasyApiTests\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This class contains the configuration information for the bundle.
+ *
+ * This information is solely responsible for how the different configuration
+ * sections are normalized, and merged.
+ *
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('easy_api_tests');
+        $rootNode
+            ->children()
+                    ->addDefaultsIfNotSet()
+                    ->booleanNode('debug')->defaultTrue()->end()
+                    ->scalarNode('datetime_format')->defaultValue(\DateTimeInterface::ATOM)->end()
+                ->end()
+            ->end()
+            ;
+
+        return $treeBuilder;
+    }
+}
---
+++ EasyApiTests.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace EasyApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class EasyApiTests extends Bundle
+{
+}
