51fe1da3fdcc71fb77d902ab10448f30339d2850
lionel <ngconnect@gmail.com>
2024-10-15 18:29:46 +0200

    fix TreeBuilder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('easy_api');
+        $treeBuilder = new TreeBuilder('easy_api_tests');
         $treeBuilder->getRootNode()
             ->children()
                     ->addDefaultsIfNotSet()
